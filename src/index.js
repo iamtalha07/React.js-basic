c5f5e2fa0fd70e4245833495e13fca11fc6ad50a
talh456 <talha.ishaq@arpatech.com>
2021-07-09 12:58:43 +0500

    Exercise #1

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import ReactDOM from 'react-dom';
                 //JSX Expression     //WHERE TO SHOW
 // ReactDOM.render(<h1>Hello World</h1>,document.getElementById('root'));
 
-
-
-
 ReactDOM.render(
-    [
-    <h1>Hello World</h1>,
-    <p>Talha Bin Ishaque</p>,
-    <h2>Arpatech</h2>
-    ],
-    document.getElementById('root'));
+    <>
+    <h1>NETFLIX</h1>
+    <p>List of of top 5 netflix shows</p>
+    <ol>
+        <li>Stranger Things.</li>
+        <li>Into the night.</li>
+        <li>The Holo Love.</li>
+        <li>Money Heist.</li>
+        <li>Dark.</li>
+    </ol>
+    </>,
+    document.getElementById('root'))
