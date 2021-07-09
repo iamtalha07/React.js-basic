3795ee663808472db57feb9f2f81ef2a91b72d74
talh456 <talha.ishaq@arpatech.com>
2021-07-09 12:05:58 +0500

    React initial

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+                //JSX Expression     //WHERE TO SHOW
+// ReactDOM.render(<h1>Hello World</h1>,document.getElementById('root'));
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    [
+    <h1>Hello World</h1>,
+    <p>Talha Bin Ishaque</p>,
+    <h2>Arpatech</h2>
+    ],
+    document.getElementById('root'));
