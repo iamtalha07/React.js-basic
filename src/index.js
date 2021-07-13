d23343ac3d798e96a80dc047d8adb8121aec9132
talh456 <talha.ishaq@arpatech.com>
2021-07-13 16:46:51 +0500

    CSS Styling and importing css files

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 
-                 //JSX Expression     //WHERE TO SHOW
-// ReactDOM.render(<h1>Hello World</h1>,document.getElementById('root'));
-
-const fname = "talha";
-const lname = "Ishaq";
-const date = new Date().toLocaleDateString();
-const time = new Date().toLocaleTimeString();
+const name = "Talha";
+const img1 = "https://picsum.photos/200/300"
+const img2 = "https://picsum.photos/250/300"
+const img3 = "https://picsum.photos/300/300"
+const link = "https://www.google.com"
 
 ReactDOM.render(
     <>
-    <h1>Hello, My Name is {fname +" "+ lname}</h1>
-    <p>Current Date = {date}</p>
-    <p>Current Time = {time}</p>
+        <h1 className="heading">My Name is {name}</h1>
+        <div className="img_div">
+        <img src={img1} alt="No Image"/>
+        <img src={img2} alt="No Image"/>
+        <a href={link}>
+        <img src={img3} alt="No Image"/>
+        </a>
+        </div>
     </>,
     document.getElementById('root')
 );
- 
